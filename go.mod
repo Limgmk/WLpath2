9cd84dffde9892f4319633ac80c3904262f28618
limgmk <limgmkgm@gmail.com>
2020-09-22 15:05:20 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module WLpath2
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		log.Fatalln("Please give a parameter")
+	}
+	parameter1 := os.Args[1]
+	if parameter1 == "-l" {
+		toLinux(os.Args[2])
+	} else if parameter1 == "-w" {
+		toWin(os.Args[2])
+	} else if parameter1 != "" {
+		toLinux(parameter1)
+	}
+}
+
+func toWin(path string) {
+	linuxPath := path
+	rootPath := ""
+	lastPath := linuxPath
+	reg := regexp.MustCompile(`^/mnt/[a-z]`)
+	if reg.MatchString(linuxPath) {
+		rootPath = fmt.Sprintf("%v:\\", strings.ToUpper(linuxPath[5:6]))
+		lastPath = linuxPath[7:]
+	}
+	reg = regexp.MustCompile(`/([A-Z]|[a-z]|[0-9]|\.)`)
+	lastPath = reg.ReplaceAllString(lastPath, "\\${1}")
+	fmt.Println(rootPath+lastPath)
+}
+
+func toLinux(path string) {
+	winPath := path
+	rootPath := ""
+	lastPath := winPath
+	reg := regexp.MustCompile(`^[A-Z]:(//|\\)`)
+	if reg.MatchString(winPath) {
+		rootPath = fmt.Sprintf("/mnt/%v/", strings.ToLower(winPath[0:1]))
+		lastPath = winPath[3:]
+		if lastPath[0:1] == "/" {
+			lastPath = lastPath[1:]
+		}
+	}
+	reg = regexp.MustCompile(`\\([A-Z]|[a-z]|[0-9]|\.)`)
+	lastPath = reg.ReplaceAllString(lastPath, "/${1}")
+	fmt.Println(rootPath+lastPath)
+}
